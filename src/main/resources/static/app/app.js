e5008c4a1cb866aa19b9e322f2dbbd7a6858a79e
Yevgen Galchenko <yevgen.galchenko@kagesoft.com>
2017-04-13 23:10:20 -0400

    UI Router

--- src/main/resources/static/app/app.js
+++ src/main/resources/static/app/app.js
@@ -1,4 +1,17 @@
 'use strict';
 (function (angular) {
-    angular.module('bands', ['bandsController', 'bandsService']);
+    angular.module('bands', ['ui.router', 'bandsController', 'bandsService']);
+    angular.module('bands').config(function($stateProvider) {
+        $stateProvider.state('bands', {
+            url: '/bands',
+            templateUrl: 'views/bands.html',
+            controller: 'bandsController'
+        }).state('new', {
+            url: '/bands/new',
+            templateUrl: 'views/band-new.html',
+            controller: 'bandsController'
+        })
+    }).run(function($state) {
+        $state.go('bands');
+    });
 }(angular));
--- src/main/resources/static/app/controllers.js
+++ src/main/resources/static/app/controllers.js
@@ -1,9 +1,13 @@
 'use strict';
 (function (angular) {
-    angular.module('bandsController', []).controller('bandsController', function ($scope, bandsService) {
+    angular.module('bandsController', []).controller('bandsController', function ($scope, $state, bandsService) {
         $scope.bands = [];
         bandsService.bands().then(function (data) {
             $scope.bands = data;
         });
+        $scope.addBand = function() {
+            alert("Implement me!");
+            $state.go('bands');
+        };
     })
 }(angular));
